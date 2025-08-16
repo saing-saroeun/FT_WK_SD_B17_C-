f73781930614f1d3171952dd9d5d5a27a9498d6d
srey khuoch <khuochsrey35@gmail.com>
2025-08-16 16:46:28 +0700

    Add project files.

---
+++ Consle_App_Framework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consle_App_Framework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
