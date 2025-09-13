c4571df17e184344db4075be46ea0c81908df401
vjk <vejaka05@gmail.com>
2025-09-13 13:23:12 +0700

    Home work 1

---
+++ Module_2-Practice/Module_2-Practice/Module_2-Practice/Errors/T05_Errors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module_2_Practice.Errors
+{
+	struct Car
+	{
+		string Make;
+		string Model;
+	}
+
+	enum Color
+	{
+		Red, Blue, Green
+	}
+
+	static void Main(string[] args)
+	{
+		Car car;
+		car.Make = "Toyota"; // ❌ ERROR: fields are private by default
+		car.Model = "Corolla";
+
+		Color myColor = "Red"; // ❌ ERROR: cannot assign string to enum
+
+		Console.WriteLine($"Car: {car.Make} {car.Model}, Color: {myColor}");
+	}
+}
